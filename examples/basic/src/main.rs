2f61e12757a5c04baa8a8ed6abfc5e70482a72f8
rojer-98 <rojermagnuson98@gmail.com>
2023-05-15 20:57:04 +0300

    [FEATURE #4] WP

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello world");
+use hh_rust::vacancies::{get_all_vacancies, get_vacancy_id};
+
+#[tokio::main]
+async fn main() {
+    if let Ok(all) = get_all_vacancies().await {
+        for v in all.items {
+            println!("Name of vacancy is {:?}", v.name);
+        }
+    }
+
+    if let Ok(v) = get_vacancy_id(80377829).await {
+        println!("Name of specific vacancy is {:?}", v.name);
+    }
 }
