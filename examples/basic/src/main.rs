8d137b0a3e33f30db76df9f7e06818d219df27f0
rojer-98 <rojermagnuson98@gmail.com>
2023-05-16 06:19:39 +0300

    [FEATURE #5] WP

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -3,18 +3,18 @@ use hh_rust::vacancies::{get_all_vacancies, get_vacancy_id, VacanciesQuery};
 #[tokio::main]
 async fn main() {
     let all_v_query = VacanciesQuery {
-        page: Some(20),
-        sort_point_lng: Some(10),
+        page: Some(5),
+        per_page: Some(20),
         ..Default::default()
     };
 
     if let Ok(all) = get_all_vacancies(Some(all_v_query)).await {
         for v in all.items {
-            println!("Name of vacancy is {:?}", v.name);
+            println!("Name of vacancy is {}={}", v.id.unwrap(), v.name.unwrap());
         }
     }
 
     if let Ok(v) = get_vacancy_id(80377829, None).await {
-        println!("Name of specific vacancy is {:?}", v.name);
+        println!("Name of specific vacancy is {}", v.name.unwrap());
     }
 }
