8bdc0e2aca382f2aa417eb73648795afec2514a3
rojer-98 <rojermagnuson98@gmail.com>
2023-05-17 06:21:00 +0300

    WP

--- src/auth/request.rs
+++ src/auth/request.rs
@@ -2,11 +2,11 @@ use log::trace;
 use serde_json::from_str;
 
 use crate::{
-    auth::{TakeAuthQuery, Token},
+    auth::Token,
     utils::{request, AuthType, HError, Header, Method, QueryHandler, RequestType},
 };
 
-pub async fn take_auth(query: Option<TakeAuthQuery>) -> Result<Token, HError> {
+async fn get_auth<T: QueryHandler>(query: Option<T>) -> Result<Token, HError> {
     let raw_url = "https://api.hh.ru/oauth/token".to_string();
 
     let url = if let Some(q) = query {
