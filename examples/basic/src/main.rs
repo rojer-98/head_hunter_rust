36541c05cf905c6ea5367203d62993638b195116
rojer-98 <rojermagnuson98@gmail.com>
2023-05-17 05:12:40 +0300

    [FEATURE #5] WP

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -1,7 +1,21 @@
-use hh_rust::vacancies::{get_all_vacancies, get_vacancy_id, VacanciesQuery};
+use hh_rust::{
+    auth::{take_auth, TakeAuthQuery},
+    vacancies::{get_all_vacancies, get_vacancy_id, VacanciesQuery},
+};
+
+use simple_logger::SimpleLogger;
+
+pub fn init_logger() {
+    let _ = SimpleLogger::new().with_utc_timestamps().init();
+}
 
 #[tokio::main]
 async fn main() {
+    init_logger();
+
+    let auth = take_auth(None).await;
+    println!("Auth is {:?}", auth);
+    /*
     let all_v_query = VacanciesQuery {
         page: Some(5),
         per_page: Some(20),
@@ -17,4 +31,5 @@ async fn main() {
     if let Ok(v) = get_vacancy_id(80377829, None).await {
         println!("Name of specific vacancy is {}", v.name.unwrap());
     }
+    */
 }
