6c38bd077c3d5868115d32aa2961fee0b7d9a4be
Roger Dubois <68107804+rojer-98@users.noreply.github.com>
2023-05-20 16:09:11 +0300

    [FEATURE #6] Advanced phones (#8)
    * [FEATURE #5] Feature advanced vacancies && Fix utils (#6) (#7)
    
    * [FEATURE #5] WP
    
    * [FEATURE #5] WP
    
    * [FEATURE #5] WP
    
    * [FEATURE #5] WP
    
    * WP
    
    * WP
    
    * [FEATURE #5] WP
    
    * [FEATURE #5] WP
    
    * [FEATURE #5] WP

--- src/phone/request.rs
+++ src/phone/request.rs
@@ -1,13 +1,23 @@
 use crate::{
     request_and_convert,
-    utils::{HError, QueryHandler},
+    utils::{HError, QueryHandler, RequestError},
 };
 
-pub async fn resume_phone_confirm<T: QueryHandler>(query: T) -> Result<(), HError> {
+
+pub async fn resume_phone_confirm<T: QueryHandler>(query: T) -> Result<RequestError, HError> {
     request_and_convert!(
         url: "https://api.hh.ru/resume_phone_confirm",
         method: POST,
         query,
-        ()
+        RequestError
+    )
+}
+
+pub async fn resume_should_send_sms<T: QueryHandler>(query: T) -> Result<RequestError, HError> {
+    request_and_convert!(
+        url: "https://api.hh.ru/resume_should_send_sms",
+        method: GET,
+        query,
+        RequestError
     )
 }
