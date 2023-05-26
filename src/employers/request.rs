2abcfb1bb77587ae7924c25f933aea5b055f002e
rojer-98 <rojermagnuson98@gmail.com>
2023-05-26 09:02:51 +0300

    [FEATURE] WP

--- src/employers/request.rs
+++ src/employers/request.rs
@@ -2,7 +2,7 @@ use std::fmt::Display;
 
 use crate::{
     dictionary::DefaultQuery,
-    employers::{TemplateQuery, TemplateResponse},
+    employers::{EmployersBrandedTemplates, EmployersDepartments, EmployersRegionActive},
     request_and_convert,
     utils::{HError, QueryHandler},
 };
@@ -11,25 +11,40 @@ pub async fn get_employers_vacancy_areas_active<T: Display>(
     access_token: Option<String>,
     employer_id: T,
     query: Option<DefaultQuery>,
-) -> Result<TemplateResponse, HError> {
+) -> Result<EmployersRegionActive, HError> {
     request_and_convert!(
         url: format!("https://api.hh.ru/employers/{employer_id}/vacancy_areas/active"),
         method: GET,
         access_token: access_token,
         optional query,
-        TemplateResponse
+        EmployersRegionActive
     )
 }
 
-pub async fn get_template<T: Display>(
+pub async fn get_employers_departments<T: Display>(
     access_token: Option<String>,
+    employer_id: T,
     query: Option<DefaultQuery>,
-) -> Result<TemplateResponse, HError> {
+) -> Result<EmployersDepartments, HError> {
     request_and_convert!(
-        url: format!("https://api.hh.ru/template"),
+        url: format!("https://api.hh.ru/employers/{employer_id}/departments"),
         method: GET,
         access_token: access_token,
         optional query,
-        TemplateResponse
+        EmployersDepartments
+    )
+}
+
+pub async fn get_branded_templates<T: Display>(
+    access_token: Option<String>,
+    employer_id: T,
+    query: Option<DefaultQuery>,
+) -> Result<EmployersBrandedTemplates, HError> {
+    request_and_convert!(
+        url: format!("https://api.hh.ru/employers/{employer_id}/vacancy_branded_templates"),
+        method: GET,
+        access_token: access_token,
+        optional query,
+        EmployersBrandedTemplates
     )
 }
--- src/employers/response.rs
+++ src/employers/response.rs
@@ -3,8 +3,46 @@ use serde::{Deserialize, Serialize};
 use crate::utils::RequestError;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct TemplateResponse {
-    pub template: Option<String>,
+pub struct EmployersRegionActive {
+    pub found: Option<i64>,
+    pub items: Option<Vec<EmployersRegionActiveItem>>,
+    pub page: Option<i64>,
+    pub pages: Option<i64>,
+    pub per_page: Option<i64>,
     #[serde(flatten)]
     pub error: Option<RequestError>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EmployersRegionActiveItem {
+    pub id: Option<String>,
+    pub name: Option<String>,
+    pub url: Option<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EmployersDepartments {
+    pub items: Option<Vec<EmployersDepartmentsItem>>,
+    #[serde(flatten)]
+    pub error: Option<RequestError>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EmployersDepartmentsItem {
+    pub id: Option<String>,
+    pub name: Option<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EmployersBrandedTemplates {
+    pub items: Option<Vec<EmployersBrandedTemplatesItem>>,
+    #[serde(flatten)]
+    pub error: Option<RequestError>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EmployersBrandedTemplatesItem {
+    pub id: Option<String>,
+    pub name: Option<String>,
+    pub version_id: Option<String>,
 }
